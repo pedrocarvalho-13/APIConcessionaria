0df12aaa45510cf0c048839b888ad6a7891a8171
pedrocarvalho-13 <pedrogui.carvalho13@gmail.com>
2024-09-01 22:50:55 -0300

    Adição da minha Migrations

---
+++ Migrations/20240902001742_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIConcessionaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EstoqueDeCarros",
+                columns: table => new
+                {
+                    Placa = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false),
+                    Fabricante = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Modelo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Ano = table.Column<int>(type: "integer", nullable: false),
+                    Quilometragem = table.Column<double>(type: "double precision", nullable: false),
+                    Situacao = table.Column<int>(type: "integer", nullable: false),
+                    Fipe = table.Column<double>(type: "double precision", nullable: false),
+                    ValorComprado = table.Column<double>(type: "double precision", nullable: false),
+                    ValorVenda = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstoqueDeCarros", x => x.Placa);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EstoqueDeCarros");
+        }
+    }
+}
---
+++ Migrations/20240902005924_UpdateSituacaoToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIConcessionaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSituacaoToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Situacao",
+                table: "EstoqueDeCarros",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Situacao",
+                table: "EstoqueDeCarros",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
