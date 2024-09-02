d5e01e9c3597ae2b5f21f610b4e29a808040fede
pedrocarvalho-13 <pedrogui.carvalho13@gmail.com>
2024-09-01 22:42:40 -0300

    Adição do AppDbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIConcessionaria
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EstoqueDeCarros> EstoqueDeCarros { get; set; }
+    }
+}
